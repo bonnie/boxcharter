16ac56b67475a85b80d86d720ac2735ea3b74142
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2016-06-20 16:55:02 -0700

    added Triangle for jinja-angular compatibility

--- server.py
+++ server.py
@@ -5,7 +5,12 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, render_template, request, flash, redirect, session
+from flask import Flask, render_template, request, flash, redirect, session, \
+                  jsonify
+
+# so jinja and angular can coexist peacably
+from flask.ext.triangle import Triangle
+
 from flask_debugtoolbar import DebugToolbarExtension
 
 from model import connect_to_db, db, User, Chart, Section, Measure, \
@@ -19,11 +24,14 @@ import os
 
 app = Flask(__name__)
 
+# for jinja / angular support
+Triangle(app)
+
 # for Flask session and debug DebugToolbar
 app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 
 # to get errors if there's an undefined Jinja variables
-app.jinja_env.undefined = StrictUndefined
+# app.jinja_env.undefined = StrictUndefined
 
 
 #########################################################
@@ -50,12 +58,25 @@ def chart_page():
     return render_template("chart_editor.html")
 
 
+@app.route('/get_chart/<int:chart_id>')
+def return_chart_json(chart_id):
+    """Return JSON object containing chart data"""
+
+    chart = Chart.query.filter_by(chart_id=chart_id)
+
+    # TODO: big-ass list/dictionary comprehension here to make object
+    sections = [section.section_name for section in chart.sections]
+
+    return jsonify({'sections': sections})
+
+
 @app.route('/save_chart')
 def save_chart():
     """Save a new or modified box chart"""
 
     # this will probably be ajax-y
     pass
+
 
 if __name__ == "__main__":
 
