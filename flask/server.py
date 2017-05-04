93fefc411064924bb127048b751d80dc2cd87452
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2017-05-03 17:42:24 -0700

    added license, removed some unnecessary cruft

--- flask/server.py
+++ flask/server.py
@@ -1,75 +1,48 @@
-"""boxcharter box chart web app server"""
+"""BoxCharter flask server to get data to angular"""
+
+# Copyright (c) 2017 Bonnie Schulkin. All Rights Reserved.
+
+# This file is part of BoxCharter.
+ 
+# BoxCharter is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+ 
+# BoxCharter is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+# for more details.
+ 
+# You should have received a copy of the GNU Affero General Public License
+# along with BoxCharter. If not, see <http://www.gnu.org/licenses/>.
+ 
 
 ######################################################
 # import
 
-from jinja2 import StrictUndefined
+from flask import Flask, jsonify
 
-from flask import Flask, render_template, request, flash, redirect, session, \
-                  jsonify
-
-# so jinja and angular can coexist peacably
-# from flask.ext.triangle import Triangle
-
-from flask_debugtoolbar import DebugToolbarExtension
-
-from model import connect_to_db, db, User, Chart, Section, Measure, \
-                  Chord, Lyric, Key, ScaleNote
-
-# for environment variables
-import os
+from chart_data import get_chart_data
 
 ######################################################
-# Flask / Jinja setup
+# Flask / setup
 
 app = Flask(__name__)
-app.jinja_env.auto_reload = True
-
-
-# for jinja / angular support
-# Triangle(app)
-
-# for Flask session and debug DebugToolbar
-app.secret_key = os.environ.get('FLASK_SECRET_KEY')
-
-# to get errors if there's an undefined Jinja variables
-# app.jinja_env.undefined = StrictUndefined
-
 
 #########################################################
 # routes
 
-@app.route('/')
-def index():
-    """Homepage"""
-
-    return render_template("index.html")
-
-
-@app.route('/register')
-def register_form():
-    """Show form for user signup"""
-
-    return render_template("register_form.html")
-
-
-@app.route('/chart_editor')
-def chart_page():
-    """Create and edit box charts"""
-
-    return render_template("chart_editor.html")
-
-
-@app.route('/get_chart/<int:chart_id>')
+@app.route('/chart/<int:chart_id>')
 def return_chart_json(chart_id):
     """Return JSON object containing chart data"""
 
-    chart = Chart.query.filter_by(chart_id=chart_id)
-
     # TODO: big-ass list/dictionary comprehension here to make object
-    sections = [section.section_name for section in enumerate(chart.sections)]
+    # sections = [section.section_name for section in enumerate(chart.sections)]
 
-    return jsonify({'chart': {'sections': sections}})
+    chart_data = get_chart_data(chart_id)
+
+    return jsonify(chart_data)
 
 
 @app.route('/save_chart', methods=['POST'])
@@ -87,24 +60,7 @@ def save_chart():
     return ''
 
 
-# no caching for testing purposes!!
-# from http://stackoverflow.com/questions/34066804/disabling-caching-in-flask
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
-
 if __name__ == "__main__":
-
-    # for testing purposes
-    app.debug = True
 
     connect_to_db(app)
 
