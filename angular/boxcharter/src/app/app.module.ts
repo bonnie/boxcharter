e10390bd782faa6b0acde00afbaddc17ad09fc13
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2017-05-03 17:01:08 -0700

    added chart and home angular components

--- angular/boxcharter/src/app/app.module.ts
+++ angular/boxcharter/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ChartComponent
   ],
   imports: [
     BrowserModule,
