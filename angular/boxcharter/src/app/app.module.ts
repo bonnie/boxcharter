f8fa843c6ef9be39c6c72f614466092b363ecb42
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2017-05-04 23:53:28 -0700

    updated app so that routing is functional

--- angular/boxcharter/src/app/app.module.ts
+++ angular/boxcharter/src/app/app.module.ts
@@ -24,12 +24,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { ClarityModule } from 'clarity-angular';
-
 import { AppComponent } from './app.component';
+import { ROUTING } from "./app.routing";
+
 import { HomeComponent } from './home/home.component';
 import { ChartComponent } from './chart/chart.component';
 import { UserComponent } from './user/user.component';
-import { ROUTING } from "./app.routing";
 
 @NgModule({
   declarations: [
@@ -39,45 +39,14 @@ import { ROUTING } from "./app.routing";
     UserComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+        BrowserAnimationsModule,
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        ClarityModule.forRoot(),
+        ROUTING // a module written by clarity
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
-// import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-// import { FormsModule } from '@angular/forms';
-// import { HttpModule } from '@angular/http';
-// import { ClarityModule } from 'clarity-angular';
-// import { AppComponent } from './app.component';
-// import { ROUTING } from "./app.routing";
-// import { HomeComponent } from "./home/home.component";
-// import { AboutComponent } from "./about/about.component";
-
-// @NgModule({
-//     declarations: [
-//         // components written by you
-//         AppComponent,
-//         AboutComponent,
-//         HomeComponent
-//     ],
-//     imports: [
-//         // components written by others (1st 4 = angular; ClarityModule = clarity)
-//         BrowserAnimationsModule,
-//         BrowserModule,
-//         FormsModule,
-//         HttpModule,
-//         ClarityModule.forRoot(),
-//         ROUTING // a module written by us
-//     ],
-//     providers: [], // array of services and dependencies for this module
-//     bootstrap: [AppComponent] 
-// })
-// export class AppModule {
-// }
