81a776e520941940a4301122d21ebacd2f49b7d3
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2017-06-08 15:34:29 -0700

    updated login route to use login-register component, instead of login component.

--- angular/boxcharter/src/app/routing/login-routing.module.ts
+++ angular/boxcharter/src/app/routing/login-routing.module.ts
@@ -24,10 +24,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardService }     from '../services/auth-guard.service';
 import { AuthService }          from '../services/auth.service';
 
-import { LoginComponent }       from '../components/login/login.component';
+import { LoginRegisterComponent } from '../components/login-register/login-register.component';
 
 const loginRoutes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginRegisterComponent }
 ];
 
 @NgModule({
