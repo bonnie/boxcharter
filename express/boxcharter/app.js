51d0ca4006a36d47bb3c2ad14cb6ac7619e0b87e
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2017-07-11 11:08:20 -0700

    Add user routes; Print error message on server error; Add licensing.

--- express/boxcharter/app.js
+++ express/boxcharter/app.js
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2017 Bonnie Schulkin. All Rights Reserved.
+ *
+ * This file is part of BoxCharter.
+ *
+ * BoxCharter is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * BoxCharter is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with BoxCharter. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -7,13 +27,13 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var model = require('./model')
 
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+// apparently necessary even though this server will only return JSON
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,7 +44,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/user', users)
+
+// The users routes
+// app.use((req, res, next) => {
+//   require('./routes/users').default(req, res, next)
+// })
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,10 +63,9 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+  console.error(err.message)
   res.status(err.status || 500);
-  res.render('error');
+
 });
 
 module.exports = app;
