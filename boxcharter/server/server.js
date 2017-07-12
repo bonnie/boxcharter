7ef9df52c7b2278aec99167698f7b06b2e7c32fd
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2017-07-11 18:22:48 -0700

    Change dist folder location to parent folder

--- boxcharter/server/server.js
+++ boxcharter/server/server.js
@@ -36,14 +36,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // Set our api routes
 app.use('/user', users);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 /**
