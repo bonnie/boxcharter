05aa10f6c017b7dc7c5b7775b0e10c0f697d8786
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2017-07-29 23:23:38 -0700

    Add functions to seed key/note/scalenote tables, and add an initial user.

---
+++ server/seed/add_scales.js
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2017 Bonnie Schulkin. All Rights Reserved.
+ *
+ * This file is part of BoxCharter.
+ *
+ * BoxCharter is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * BoxCharter is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with BoxCharter. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/////////////////////////////////////////////////////////////////////
+// Add notes and scales. To be done once when tables are created.
+/////////////////////////////////////////////////////////////////////
+
+fs = require('fs')
+
+const Key = require('../model/note-key').Key
+const Note = require('../model/note-key').Note
+const ScaleNote = require('../model/note-key').ScaleNote
+const db = require('../model/db')
+require('../model/associations.js')
+
+
+const addNotes = function() {
+  // Add all the notes to the db, so they'll be there for the keys
+  // With Sequelize, it's easier this way than checking as we go along.
+
+  const NOTES = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+  const ACC = ['b', '', '#']
+  NOTES.map(baseNote => {
+    ACC.map(acc => {
+      note = baseNote + acc
+      Note.create({noteCode: note})
+        .then(n => {console.log(`Created note ${n.noteCode}`)})
+        .catch(err => {console.log(`Failed to create note ${n.noteCode}: ${err}`)})
+    })
+  })
+}
+
+
+const addScales = function() {
+  fs.readFile('../seed/keys.csv', 'ascii', (err, data) => {
+    if (err) { return console.log(err); }
+
+    // othersiwe, parse the data and add keys to db
+    const keys = {}
+    data.split('\n').map(keyLine => {
+
+        // example line: Ab,Bb,C,Db,Eb,F,G
+        const notes = keyLine.split(',')
+        const tonic = notes[0]
+
+        const scale_notes = notes.slice(1).map(
+          (note, index) => {
+            return { noteCode: note, scaleDegree: index }
+          })
+
+        // create key
+        Key.create(
+          {
+            keyCode: tonic,
+            scale_notes: scale_notes
+          },
+          {
+            include: [ScaleNote]
+        })
+        .then(k => {console.log(`created key ${k.keyCode}`)})
+        .catch(err => {console.log(`could not create key: ${err}`)})
+      })
+    })
+}
+
+const addNotesAndScales = function () {
+  addNotes()
+  addScales()
+}
+
+module.exports = addNotesAndScales
---
+++ server/seed/add_user.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2017 Bonnie Schulkin. All Rights Reserved.
+ *
+ * This file is part of BoxCharter.
+ *
+ * BoxCharter is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * BoxCharter is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with BoxCharter. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+//////////////////////////////////////////////////////////////////////////
+// Create an initial test user
+//////////////////////////////////////////////////////////////////////////
+
+var User = require('../model/user')
+var passUtils = require('../utilities/password_utils')
+var checkPass = require('../utilities/password_utils').checkPass
+
+const userInfo = { email: 'bonnie@bonnie',
+  fname: 'Bonnie',
+  lname: 'BoBonnie',
+  password: 'bonnie',
+  password2: 'bonnie' }
+
+const addUser = function() {
+
+  // create salted password hash
+  // general web consensus seems to be: hash on server side, and use
+  // https to protect password in transit
+  const passData = passUtils.saltHashPassword(userInfo.password)
+
+  // massage userInfo obj to contain salted pass hash
+  userInfo.passwordHash = passData.passwordHash
+  userInfo.passwordSalt = passData.salt
+  delete userInfo.password
+
+  // create user
+  User.create(userInfo)
+    .then(newUser => {
+      console.log(`New user ${newUser.email} successfully added.`)
+    })
+    .catch(error => {
+      console.log(`Unable to add user ${userInfo.email}`)
+    })
+}
+
+module.exports = addUser
