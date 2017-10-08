cb8df8f1f2eaf047f21421bbbf53a95d6e162d12
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2017-10-08 11:30:17 -0700

    Clean up syntax for JSDoc and ESLint

--- shared/model/status.js
+++ shared/model/status.js
@@ -18,25 +18,41 @@
  *
  */
 
-class Status {
-  constructor(type, msg) {
-    this.alertType = type
-    this.text = msg
-    this.clrAlertClosed = false // for ui
-  }
-}
+/**
+ * Class for status passed from server to client.
+ * @module status
+ */
 
 const adminEmail = 'admin@boxcharter.com'
 const contactAdmin = `Please report this error to ${adminEmail}`
 
 const statusStrings = {
-    success: 'alert-success',
-    warning: 'alert-warning',
-    danger: 'alert-danger',
-    contactAdmin: contactAdmin,
-    badRequest: `The server did not understand this request. ${contactAdmin}`
+  success: 'alert-success',
+  warning: 'alert-warning',
+  danger: 'alert-danger',
+  badRequest: `The server did not understand this request. ${contactAdmin}`,
+  contactAdmin,
+}
+
+/**
+ * Status object.
+ * @class
+ */
+class Status {
+  /**
+   * User constructor
+   * @constructor
+   * @param {string} type - type of alert for front end
+   * @param {string} msg - message for the allert
+   */
+  constructor(type, msg) {
+    this.alertType = type
+    this.text = msg
+    this.clrAlertClosed = false // used by client
+  }
 }
 
 module.exports = {
-  Status: Status,
-  statusStrings: statusStrings }
+  Status,
+  statusStrings,
+}
