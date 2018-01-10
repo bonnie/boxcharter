a392bf0cc3f5faae0bd5ff1120384f978b683f8e
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2018-01-09 20:18:41 -0800

    Updated date not to show year if it matches current year

--- client/src/components/formatted_date.js
+++ client/src/components/formatted_date.js
@@ -5,7 +5,16 @@ import moment from 'moment'
  * Simple functional react component to format a date
  */
 export default (props) => {
-  const format = props.format || 'MMM D, YYYY h:mm a'
+  let format
+  if (props.format) {
+    format = props.format
+  } else {
+    // leave off year if it's this year
+    const currentYear = new Date().getFullYear().toString()
+    const dateYear = moment(props.date).format('YYYY')
+    const year = (dateYear === currentYear) ? '' : 'YYYY '
+    format = `MMM D, ${year}h:mm a`
+  }
   const formattedDate = moment(props.date).format(format)
   return (<span className="formatted-date">{formattedDate}</span>)
 }
