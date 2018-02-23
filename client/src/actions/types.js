4f893aa66ed90ec7da0cfc382310291beee182ff
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2018-02-23 14:20:44 -0800

    Move actionTypes into app dir

--- client/src/actions/types.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  AUTH_ERROR: 'auth_error',
-  AUTH_USER: 'auth_user',
-  GET_CHART: 'get_chart',
-  GET_USERCHARTS: 'get_usercharts',
-  NO_ACTION: 'no_action',
-  UNAUTH_USER: 'unauth_user',
-}
